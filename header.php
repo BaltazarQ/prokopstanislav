e6b888a92cd6ca80fea7a3e2ae3d4c25846974d6
Stanislav Prokop <prokopstanislav@gmail.com>
2023-11-14 15:09:50 +0100

    Oprava

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
                     <li id="menu-preface"><a href="index.php">Domov</a></li>
                     <li id="menu-profile"><a href="#profile">Profil</a></li>
                     <li id="menu-about"><a href="#about">Kto som</a></li>
-                    <!-- <li id="menu-more"><a href="#more">Viac o mne</a></li> -->
+                    <li id="menu-more"><a href="#more">Viac o mne</a></li>
                     <li id="menu-contact"><a href="#contact">Kontakt</a></li>
                 </ul>
                 <div class="scrolling-line"></div>
