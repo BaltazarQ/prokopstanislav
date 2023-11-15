acc39b3e046787e1a3bb0064ee641ae6c936a792
Stanislav Prokop <prokopstanislav@gmail.com>
2023-11-15 16:47:53 +0100

    Create menu on left side

--- header.php
+++ header.php
@@ -3,11 +3,10 @@
         <li class="menu-icon"><i class=" horizontal fas fa-bars"></i>
             <div class="navigation-wrap">
                 <ul class="navigation">
-                    <li id="menu-preface"><a href="index.php">Domov</a></li>
-                    <li id="menu-profile"><a href="#profile">Profil</a></li>
-                    <li id="menu-about"><a href="#about">Moje práce</a></li>
-                    <!-- <li id="menu-more"><a href="#more">Viac o mne</a></li> -->
-                    <li id="menu-contact"><a href="#contact">Kontakt</a></li>
+                    <li id="menu-about"><a href="#about" data-value="O mne">O mne</a></li>
+                    <li id="menu-profile"><a href="#profile" data-value="Profil">Profil</a></li>
+                    <li id="menu-works"><a href="#works" data-value="Moje práce">Moje práce</a></li>
+                    <li id="menu-contact"><a href="#contact" data-value="Kontakt">Kontakt</a></li>
                 </ul>
                 <div class="scrolling-line"></div>
             </div>
