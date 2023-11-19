5cce7bca10ed442bc567d2a68a2ac366077807fa
Stanislav Prokop <prokopstanislav@gmail.com>
2023-11-19 11:24:21 +0100

    Edit main sections - HTML, Sass

--- main.php
+++ main.php
@@ -1,73 +1,26 @@
 <section class="preface" id="preface">
-    <!-- <div class="row"> -->
-        <article class="profile-photo">
-            <img src="images/profil-square.jpg" alt="rounded profil photo">
-        </article>
-        <article class="preface-info">
-            <h1 class="name">Stanislav Prokop</h1>
-            <h3 class="position">Web developer</h3>
-            <!-- <div class="preface-wrap">
-                <strong>Vek:</strong>
-                <p class="main-text">38</p>
-            </div>
-            <div class="preface-wrap">
-                <strong>Mesto:</strong>
-                <p class="main-text">Bardejov</p>
-            </div> -->
-            <!-- <p class="main-text">Som mladý, optimisticky naladený muž, ktorý sa nebojí nových výziev. Na veci sa pozerám z viacerých strán. Rád trávim čas s priateľmi a rodinou. Mám rád úprimnosť, humor a bryndzové halušky. Zaujíma ma história, aj technologické novinky.</p> -->
-        </article>
-        <!-- <div class="preface-right"> -->
-            <!-- <img src="images/preface.jpg" alt="profilová foto Stanislav Prokop">
-        </div> -->
-    <!-- </div> -->
-    <!-- <div class="btn">
-        <a href="#profile">Viac</a>
-    </div> -->
+    <article class="profile-photo">
+        <img src="images/profil-square.jpg" alt="rounded profil photo">
+    </article>
+    <article class="preface-info">
+        <h1 class="name">Stanislav Prokop</h1>
+        <h2 class="position">Web developer</h2>
+    </article>
 </section>
 
 <section class="profile" id="profile">
-    <h1>Profil</h1>
-    <h4>začínajúci frontend developer</h4>
-    
     <div class="profile-row">
         <article class="profile-left">
             <h3>O mne</h3>
             <p class="main-text">Študoval som na Strednej priemyselnej škole, po ktorej som pokračoval v štúdiu na Stavebnej fakulte v Bratislave, neskôr externe v Košiciach. Prešiel som si viacerými pracovnými odvetviami, kde som mal možnosť vyskúšať veľa nových veci, stretnúť množstvo ľudí, aj naučiť sa viacerým technológiám. </p>
         </article>
-        
-        <article class="profile-wrap">
-            <div class="profile-middle">
-                <img src="images/profil-square.jpg" alt="profilova fotka">
-                
-            </div>
-            <div class="profile-right">
-                <h3>Detaily</h3>
-                <strong>Meno:</strong>
-                <p>Stanislav Prokop</p>
-                <strong>Vek</strong>
-                <p>36 rokov</p>
-                <strong>Mesto:</strong>
-                <p>Bardejov</p>
-            </div>
-        </article>
-    </div>
-    <div class="btn">
-        <a href="#about">Viac</a>
-    </div>
-</section>
 
-<section class="about" id="about">
-    <div class="about-container">
-        <h1>Kto som</h1>
-        <article>
-            <div class="about-text">    
-                <p>​​S prvým kódom som sa stretol v decembri 2019, kedy som zistil, že to nemusí byť raketová veda :) Odvtedy vo voľnom čase napredujem v štúdiu tvorby webových stránok. Ako podklady mi slúžia rôzne videokurzy, dokumentácia k jednotlivým technológiam aj rôzne rady skúsenejších programátorov. Najviac sa však človek reálne učí vlastnou praxou.</p>
-                <p>Mojim najbližším cieľom je stať sa plnohodnotným frontenďákom a učiť sa nové a zdokonaľovať terajšie znalosti</p>
-            </div>
+        <article class="profile-wrap">
+            <h3>Detaily</h3>
             <aside class="aside-contact">
-                <strong>Email</strong>
+                    <p>37 rokov</p>
+                    <p>Bardejov, Slovensko</p>
                 <p><a href="mailto:prokopstanislav@gmail.com">prokopstanislav@gmail.com</a></p>
-                <strong>Telefón</strong>
                 <p><a href=" tel:+421904744404">+421 904 744 404</a></p>
                 <div class="social-icon">
                     <a class="social-fb" href="https://www.facebook.com/stanislav.prokop.50/" target="blank"><i class="fab fa-facebook-f"></i></a>
@@ -77,64 +30,65 @@
                     <a class="social-git" href="https://github.com/BaltazarQ" target="blank"><i class="fab fa-github"></i></a>
                 </div>
             </aside>
+            </div>
         </article>
     </div>
+</section>
 
-    <div class="aside-work">
-        <h3>Z mojich prác</h3>
-        <div class="presentation">
-            
-            <div class="presentation-item">
-                <div class="presentation-family-game">
-                    <a href="family-game\index.php" target="blank">
-                        <img src="images/family-game.png" alt="">
-                    </a>
-                    <p>Quiz</p>
-                    <p class="tech-list">HTML/PHP, CSS/Sass, Javascript, jQuerry</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/family-game">GitHub</a></p>
-                </div>
-                <!-- <div class="tech-family">
-                    <h5>Použité technológie</h5>
-                </div> -->
+<section class="about" id="about">
+    <div class="about-container">
+        <h3>Moje skúsenosti</h3>
+        <article>
+            <div class="about-text">    
+                <p>​S kódom som sa stretol v roku 2020, keď som začal studovať HTML, CSS, Javascript, jQuerry, neskôr som prešiel na Sass, PHP a SQL.</p>
+                <p>Od januára 2023 som mal možnosť pracovať takmer rok ako Junior programátor PHP, kde som mal možnosť vyskúšať si aj prácu s Magento 2.</p>
             </div>
 
-            <div class="presentation-item">
-                <div class="presentation-fiato-rosa">
-                    <a href="fiato_rosa\index.php" target="blank">
-                        <img src="images/fiato-rosa.png" alt="">
-                    </a>
-                    <p>Fiato Rosa</p>
-                    <p class="tech-list">HTML/PHP, CSS/Sass, SQL, Javascript</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/fiato-rosa">GitHub</a></p>
-                    <!-- <div class="tech-fiato">
-                        <h5>Použité technológie</h5>
-                    </div> -->
-                </div>
-            </div>
-            
-            <div class="presentation-item">
-                <div class="presentation-gallery">        
-                    <a href="moja_stranka/index.php" target="blank">
-                        <img src="images/moja-stranka.png" alt="">
-                    </a>
-                    <p>My Family</p>
-                    <p class="tech-list">HTML/PHP, CSS, SQL, Javascript/jQuerry</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/moja-stranka">GitHub</a></p>
-                </div>
-                <!-- <div class="tech-gallery">
-                    <h5>Použité technológie</h5>
-                </div> -->
-            </div>
-
-        </div>
-    </div>
-    <div class="btn">
-        <a href="#more">Viac</a>
+        </article>
     </div>
 </section>
 
 <section class="more" id="more">
-    <h1>Viac o mne</h1>
+
+<div class="aside-work">
+        <h3>Z mojich prác</h3>
+        <div class="presentation">
+            
+            <div class="presentation-item">
+                <!-- <div class="presentation-family-game"> -->
+                    <a href="family-game\index.php" target="blank">
+                        <img src="images/family-game.png" alt="">
+                    </a>
+                    <span>Quiz</span>
+                    <p class="tech-list">HTML/PHP, CSS/Sass, Javascript, jQuerry</p>
+                    <p><a class="git" href="https://github.com/BaltazarQ/family-game">GitHub</a></p>
+                <!-- </div> -->
+            </div>
+
+            <div class="presentation-item">
+                <!-- <div class="presentation-fiato-rosa"> -->
+                    <a href="fiato_rosa\index.php" target="blank">
+                        <img src="images/fiato-rosa.png" alt="">
+                    </a>
+                    <span>Fiato Rosa</span>
+                    <p class="tech-list">HTML/PHP, CSS/Sass, SQL, Javascript</p>
+                    <p><a class="git" href="https://github.com/BaltazarQ/fiato-rosa">GitHub</a></p>
+                <!-- </div> -->
+            </div>
+            
+            <div class="presentation-item">
+                <!-- <div class="presentation-gallery">         -->
+                    <a href="moja_stranka/index.php" target="blank">
+                        <img src="images/moja-stranka.png" alt="">
+                    </a>
+                    <span>My Family</span>
+                    <p class="tech-list">HTML/PHP, CSS, SQL, Javascript/jQuerry</p>
+                    <p><a class="git" href="https://github.com/BaltazarQ/moja-stranka">GitHub</a></p>
+                <!-- </div> -->
+            </div>
+        </div>
+    </div>
+    <!-- <h1>Viac o mne</h1>
     <p>Ak sa chceš o mne dozvedieť viac, klikni na oblasť ktorá ťa zaujíma</p>
     <div class="curiculum">
         <ol class="cv">
@@ -179,13 +133,13 @@
     <p class="git-link">Kód tejto stránky si môžete pozrieť na mojom GitHube <a href="https://github.com/BaltazarQ">TU!</a></p>
     <div class="btn">
         <a href="#contact">Viac</a>
-    </div>
+    </div> -->
 </section>
 
 <section class="contact" id="contact">
     <div class="contact-left">
-        <p>Zaujal som Vás?</p>
         <h3>Kontaktujte ma</h3>
+        <p>Zaujal som Vás?</p>
         <p>Ak sa Vám moja práca páči, rád sa s Vami stretnem osobne. Verím, že naša spolupráca prospeje ako mne, tak aj Vám a budeme spoločne napredovať. Som otvorený novým možnostiam a zároveň sa rád naučím aj nové veci.</p>
     </div>
 
