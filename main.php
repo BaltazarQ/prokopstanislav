268193e7d1e51cebeaba25eb97cf6b27fb7b8de6
Stanislav Prokop <prokopstanislav@gmail.com>
2023-11-30 10:00:55 +0100

    Edit media queries and classes name

--- main.php
+++ main.php
@@ -11,18 +11,18 @@
     </article>
 </section>
 
-<section class="profile" id="about">
-    <div class="profile-row">
-        <article class="profile-left">
+<section class="about" id="about">
+    <div class="about-row">
+        <article class="about-left">
             <h3>O mne</h3>
             <p class="main-text">Študoval som na Strednej priemyselnej škole, po ktorej som pokračoval v štúdiu na Stavebnej fakulte v Bratislave, neskôr externe v Košiciach. Prešiel som si viacerými pracovnými odvetviami, kde som mal možnosť vyskúšať veľa nových veci, stretnúť množstvo ľudí, aj naučiť sa viacerým technológiám. </p>
         </article>
 
-        <article class="profile-wrap">
+        <article class="about-wrap">
             <h3>Detaily</h3>
             <aside class="aside-contact">
-                    <p>37 rokov</p>
-                    <p>Bardejov, Slovensko</p>
+                <p>37 rokov</p>
+                <p>Bardejov, Slovensko</p>
                 <p><a href="mailto:prokopstanislav@gmail.com">prokopstanislav@gmail.com</a></p>
                 <p><a href=" tel:+421904744404">+421 904 744 404</a></p>
                 <div class="social-icon">
@@ -33,110 +33,56 @@
                     <a class="social-git" href="https://github.com/BaltazarQ" target="blank"><i class="fab fa-github"></i></a>
                 </div>
             </aside>
-            </div>
+            <!-- </div> -->
         </article>
     </div>
 </section>
 
-<section class="about" id="profile">
-    <div class="about-container">
+<section class="profile" id="profile">
+    <div class="profile-container">
         <h3>Moje skúsenosti</h3>
         <article>
-            <div class="about-text">    
+            <div class="profile-text">    
                 <p>​S kódom som sa stretol v roku 2020, keď som začal studovať HTML, CSS, Javascript, jQuerry, neskôr som prešiel na Sass, PHP a SQL.</p>
                 <p>Od januára 2023 som mal možnosť pracovať takmer rok ako Junior programátor PHP, kde som mal možnosť vyskúšať si aj prácu s Magento 2.</p>
             </div>
-
         </article>
     </div>
 </section>
 
-<section class="more" id="works">
-
-<div class="aside-work">
+<section class="works" id="works">
+    <div class="aside-work">
         <h3>Z mojich prác</h3>
         <div class="presentation">
             
             <div class="presentation-item">
-                <!-- <div class="presentation-family-game"> -->
-                    <a href="family-game\index.php" target="blank">
-                        <img src="images/family-game.png" alt="">
-                    </a>
-                    <span>Quiz</span>
-                    <p class="tech-list">HTML/PHP, CSS/Sass, Javascript, jQuerry</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/family-game">GitHub</a></p>
-                <!-- </div> -->
+                <a href="family-game\index.php" target="blank">
+                    <img src="images/family-game.png" alt="">
+                </a>
+                <span>Quiz</span>
+                <p class="tech-list">HTML/PHP, CSS/Sass, Javascript, jQuerry</p>
+                <p><a class="git" href="https://github.com/BaltazarQ/family-game">GitHub</a></p>
             </div>
 
             <div class="presentation-item">
-                <!-- <div class="presentation-fiato-rosa"> -->
-                    <a href="fiato_rosa\index.php" target="blank">
-                        <img src="images/fiato-rosa.png" alt="">
-                    </a>
-                    <span>Fiato Rosa</span>
-                    <p class="tech-list">HTML/PHP, CSS/Sass, SQL, Javascript</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/fiato-rosa">GitHub</a></p>
-                <!-- </div> -->
+                <a href="fiato_rosa\index.php" target="blank">
+                    <img src="images/fiato-rosa.png" alt="">
+                </a>
+                <span>Fiato Rosa</span>
+                <p class="tech-list">HTML/PHP, CSS/Sass, SQL, Javascript</p>
+                <p><a class="git" href="https://github.com/BaltazarQ/fiato-rosa">GitHub</a></p>
             </div>
             
             <div class="presentation-item">
-                <!-- <div class="presentation-gallery">         -->
-                    <a href="moja_stranka/index.php" target="blank">
-                        <img src="images/moja-stranka.png" alt="">
-                    </a>
-                    <span>My Family</span>
-                    <p class="tech-list">HTML/PHP, CSS, SQL, Javascript/jQuerry</p>
-                    <p><a class="git" href="https://github.com/BaltazarQ/moja-stranka">GitHub</a></p>
-                <!-- </div> -->
+                <a href="moja_stranka/index.php" target="blank">
+                    <img src="images/moja-stranka.png" alt="">
+                </a>
+                <span>My Family</span>
+                <p class="tech-list">HTML/PHP, CSS, SQL, Javascript/jQuerry</p>
+                <p><a class="git" href="https://github.com/BaltazarQ/moja-stranka">GitHub</a></p>
             </div>
         </div>
     </div>
-    <!-- <h1>Viac o mne</h1>
-    <p>Ak sa chceš o mne dozvedieť viac, klikni na oblasť ktorá ťa zaujíma</p>
-    <div class="curiculum">
-        <ol class="cv">
-            <li data-from="left">
-                <a id="web-dev" href="web.php">
-                    <p>Web skills</p>
-                </a>
-            </li>
-
-            <li data-from="right">
-                <a id="study" href="study.php">
-                    <p>Štúdium</p>
-                </a>
-            </li>
-
-            <li data-from="left">
-                <a id="work" href="work.php">
-                    <p>Práca</p>
-                </a>
-            </li>
-
-            <li data-from="right">
-                <a id="skills" href="skills.php">
-                    <p>Zručnosti</p>
-                </a>
-            </li>
-
-            <li data-from="right">
-                <a id="traits" href="traits.php">
-                    <p>Vlastnosti</p>
-                </a>
-            </li>
-            
-            <li data-from="left">
-                <a id="hobby" href="hobby.php">
-                    <p>Voľný čas</p>
-                </a>
-            </li>
-        </ol>
-    </div>
-    <p class="cv-pdf">Celý môj životopis si môžete stiahnuť aj vo formáte PDF <a href="CV_Prokop.pdf">TU!</a></p>
-    <p class="git-link">Kód tejto stránky si môžete pozrieť na mojom GitHube <a href="https://github.com/BaltazarQ">TU!</a></p>
-    <div class="btn">
-        <a href="#contact">Viac</a>
-    </div> -->
 </section>
 
 <section class="contact" id="contact">
